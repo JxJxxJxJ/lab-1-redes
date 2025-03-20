621c13dd5cb635305dd877c7cbfd3d583b5c5185
Ferca <fercabreraluque@gmail.com>
2025-03-20 04:01:32 -0300

    feat: sugerir_pelicula_aleatoria() y sugerir_pelicula_aleatoria_por_genero(genero) hechos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import random
 from proximo_feriado import NextHoliday
+import unicodedata  # Para normalizar los géneros (de momento)
 
 
 
@@ -22,12 +23,12 @@ peliculas = [ ## Esto es un json <-- ???
 ]
 
 
-# @app.get("/") # Usamos esto en vez de la linea 23
+@app.get("/") # Usamos esto en vez de la linea 23
 def obtener_peliculas():
     return peliculas
 
 
-#@app.get("/peliculas/<int:id>")
+@app.get("/peliculas/<int:id>")
 def obtener_pelicula(id):
     # Lógica para buscar la película por su ID y devolver sus detalles
 
@@ -35,13 +36,13 @@ def obtener_pelicula(id):
     for pelicula in peliculas:
         if pelicula['id'] == id:
             pelicula_encontrada = pelicula
-            return jsonify(pelicula_encontrada)
+            return jsonify(pelicula_encontrada), 200
 
     # Si la película no está en la DB, envía el siguiente mensaje
-    return jsonify({'mensaje': 'Película no encontrada'}), 404 # Error 404
+    return jsonify({'mensaje': 'Película no encontrada'}), 404
 
 
-# @app.post("/peliculas")
+@app.post("/peliculas")
 def agregar_pelicula():
     nueva_pelicula = {
         'id': obtener_nuevo_id(),
@@ -53,7 +54,7 @@ def agregar_pelicula():
     return jsonify(nueva_pelicula)
 
 
-# @app.put("/peliculas/<int:id>")
+@app.put("/peliculas/<int:id>")
 def actualizar_pelicula(id):
     # Lógica para buscar la película por su ID y actualizar sus detalles
 
@@ -65,13 +66,13 @@ def actualizar_pelicula(id):
 
             # Si la encuentra, la película se actualiza
             pelicula_actualizada = pelicula_a_actualizar
-            return jsonify(pelicula_actualizada)
+            return jsonify(pelicula_actualizada), 200
     
     # En caso de que la película no esté en la DB, envía el siguiente mensaje
     return jsonify({'mensaje': 'Película no encontrada'}), 404
 
 
-# @app.delete("/peliculas/<int:id>")
+@app.delete("/peliculas/<int:id>")
 def eliminar_pelicula(id):
     # Lógica para buscar la película por su ID y eliminarla
 
@@ -79,7 +80,7 @@ def eliminar_pelicula(id):
     for pelicula_a_eliminar in peliculas:
         if pelicula_a_eliminar['id'] == id:
             peliculas.remove(pelicula_a_eliminar)
-            return jsonify({'mensaje': 'Película eliminada correctamente'})
+            return jsonify({'mensaje': 'Película eliminada correctamente'}), 200
     
     # En caso de fallo, devuelve la siguiente advertencia
     return jsonify({'mensaje': 'No se pudo eliminar la película correctamente'}), 404
@@ -93,11 +94,67 @@ def obtener_nuevo_id():
         return 1
 
 
+# Posiblemente implementar algo para el caso de que no hayan películas en la
+# DB
+@app.get("/peliculas")
+def sugerir_pelicula_aleatoria():
+    # random escoge una película cualquiera del json de películas
+    pelicula_aleatoria = random.choice(peliculas)
+    return jsonify(pelicula_aleatoria), 200
+
+
+def normalizar_texto(texto):
+    # Convierte el texto a minúsculas
+    texto = texto.lower()
+
+    # NFKD separa las tildes de las letras. Por ejemplo 'á' => 'a' + '´'
+    # Se codifica el texto en ASCII, descartando caracteres especiales y se
+    # decodifica en UTF-8 para tener la cadena normalizada
+    texto = unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').decode('utf-8')
+    return texto
+
+
+@app.get("/recomendacion/<string:genero>")
+def sugerir_pelicula_aleatoria_por_genero(genero):
+    # Guardamos el input del usuario para usarlo luego en caso de que no exis-
+    # tan películas de dicho género en la DB
+    genero_inicial = genero
+
+    # En caso de haber mandado 'ciencia_ficcion' o 'thriller-policial' como 
+    # género, por ejemplo
+    genero = genero.replace("-", " ")
+    genero = genero.replace("_", " ")
+
+    # Normalizamos el género para aceptar varias posibilidades como: 
+    #   - ACCION
+    #   - accion
+    #   - cÍÉncÍÁ%20fÍccÍÓn
+    #   - aVeNtUrA
+    #   - y otras posibles combinaciones bizarras de caracteres...
+    genero = normalizar_texto(genero) 
+
+    # Arma una lista compuesta únicamente con las películas del género dado.
+    # Vale aclarar que compara ambos géneros ya normalizados, tanto el ingresa-
+    # do por el usuario como el del json
+    peliculas_filtradas = [
+        p for p in peliculas if normalizar_texto(p['genero']) == genero
+    ]
+
+    # Si la lista está vacía, significa que no hay películas de tal género y se
+    # envía una advertencia. Caso contrario, random escoge una película cual-
+    # quiera de la lista previamente creada
+    if peliculas_filtradas:
+        pelicula_elegida = random.choice(peliculas_filtradas)
+        return jsonify(pelicula_elegida), 200 
+    else:
+        return jsonify({'error': f'No existen películas del género {genero_inicial}'}), 404
+        
+
 """
     Aca abajo esta lo necesario para la parte b) del modulo 2
 """
 
-# @app.get("/recomendacion")
+@app.get("/recomendacion")
 def recomendar():
     # Capturo el valor de la key json (ej: Crimen)
     genero = request.json['genero']
@@ -118,11 +175,28 @@ def recomendar():
     return jsonify(output)
 
 
-app.add_url_rule('/peliculas', 'obtener_peliculas', obtener_peliculas, methods=['GET'])
-app.add_url_rule('/peliculas/<int:id>', 'obtener_pelicula', obtener_pelicula, methods=['GET'])
-app.add_url_rule('/peliculas', 'agregar_pelicula', agregar_pelicula, methods=['POST'])
-app.add_url_rule('/peliculas/<int:id>', 'actualizar_pelicula', actualizar_pelicula, methods=['PUT'])
-app.add_url_rule('/peliculas/<int:id>', 'eliminar_pelicula', eliminar_pelicula, methods=['DELETE'])
+# app.add_url_rule(
+#     '/peliculas', 'obtener_peliculas', obtener_peliculas, methods=['GET']
+# )
+# app.add_url_rule(
+#     '/peliculas/<int:id>', 'obtener_pelicula', obtener_pelicula, methods=['GET']
+# )
+# app.add_url_rule(
+#     '/peliculas', 'agregar_pelicula', agregar_pelicula, methods=['POST']
+# )
+# app.add_url_rule(
+#     '/peliculas/<int:id>', 
+#     'actualizar_pelicula', 
+#     actualizar_pelicula, 
+#     methods=['PUT']
+# )
+# app.add_url_rule(
+#     '/peliculas/<int:id>', 
+#     'eliminar_pelicula', 
+#     eliminar_pelicula, 
+#     methods=['DELETE']
+# )
+
 
 if __name__ == '__main__':
     app.run()
